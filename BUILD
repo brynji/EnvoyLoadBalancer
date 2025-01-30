c4f446486607438b5ad5ba82a4aa0953ea2e3111
Jiří Brynda <bryndji3@fit.cvut.cz>
2025-01-30 17:14:43 +0100

    Added everything required for successful compilation

--- BUILD
+++ BUILD
@@ -2,9 +2,7 @@ package(default_visibility = ["//visibility:public"])
 
 load(
     "@envoy//bazel:envoy_build_system.bzl",
-    "envoy_cc_binary",
-    "envoy_cc_library",
-    "envoy_cc_test",
+    "envoy_cc_binary"
 )
 
 envoy_cc_binary(
--- service.py
+++ service.py
@@ -1,12 +1,16 @@
 from aiohttp import web
 import sys
+import datetime
 
 if len(sys.argv)<2:
 	print("Port number is missing")
 	exit()
-
+if not sys.argv[1].isnumeric():
+	print("Port number is not numeric")
+	exit()
+	
 async def hello(request):
-    return web.Response(text="Hello, world from "+sys.argv[1])
+    return web.Response(text="Hello, world from "+sys.argv[1] + "\nLocal time is "+ str(datetime.datetime.now()))
 
 
 app = web.Application()
